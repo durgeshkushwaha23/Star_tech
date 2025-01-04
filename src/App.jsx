8acd45930917b9c77fa6576c65e3e8f1cdf0277f
durgeshkushwaha23 <durgeshkushwaha415@gmail.com>
2025-01-04 20:08:49 +0530

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Structure from './comonents/structure/Structure'
+
+const App = () => {
+  return (
+    <div>
+
+      <Structure/>
+    </div>
+  )
+}
+
+export default App
